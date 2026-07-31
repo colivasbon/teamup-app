2a9919d6e41c2e7150e49e5cb195c39ff3ac2d60
colivasbon <colivasbon@gmail.com>
2026-07-31 11:59:48 +0200

    v14.1.15

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -121,7 +121,7 @@ export default function Navbar() {
       )}
 
       <nav className="navbar">
-
+        <div className="navbar-glass" />
         {/* ── Iconos de navegación ── */}
         <div className="nav-inner">
           {NAV_ITEMS.map(({ href, label, icon }) => {
